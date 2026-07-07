c72d0ed80e451862d002582576527121eec3cfa4
lucas_correa14@estudante.sesisenai.org.br <lucas_correa14@estudante.sesisenai.org.br>
2026-07-07 17:06:49 -0300

    add - comecei a fazer o ex_05

---
+++ ex_05.php
@@ -0,0 +1,12 @@
+<?php
+
+function analisarTexto(){
+  $texto = "Uma das coisas que aprendi é que se deve viver apesar de. Apesar de, se deve comer.
+            Apesar de, se deve amar. Apesar de, se deve morrer. Inclusive muitas vezes é o pró-
+            prio apesar de que nos empurra para a frente. Foi o apesar de que me deu uma angús-
+            tia que insatisfeita foi a criadora de minha própria vida. Foi o apesar de que par-
+            ei na rua e fiquei olhando para você enquanto você esperava um táxi. Clarice Lispe-
+            ctor";
+
+  
+}
