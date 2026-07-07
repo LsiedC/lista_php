0b1efa6e03d16acc962b4c490e2bf0dd9617f027
lucas_correa14@estudante.sesisenai.org.br <lucas_correa14@estudante.sesisenai.org.br>
2026-07-07 16:46:22 -0300

    add - exercício três resolvido

---
+++ ex_03.php
@@ -0,0 +1,16 @@
+<?php
+
+function mascararCpf($cpf){
+    
+  $cpfLimpo = preg_replace('/\D/', '', $cpf);
+    if (strlen($cpfLimpo) !== 11) {        // Verifica se o cpf tem 11 caracteres sem o formato
+        return "Seu CPF é inválido!";
+    }
+    
+    $final = substr($cpfLimpo, -4);       // Aqui ele pega os ultimos 4 numeros do cpf
+    
+    return "*******" . $final;
+}
+echo mascararCpf("123.456.789-00"); // Echo serve para retornar o cpf já mascarado *******8900
+
+?>
