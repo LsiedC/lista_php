91973183a8471dec0b33f9123996f12705aed33b
LsiedC <lucas_correa14@estudante.sesisenai.org.br>
2026-07-08 10:13:39 -0300

    add - comecei o ex_09

---
+++ exercícios/ex_09.php
@@ -0,0 +1,16 @@
+<!-- Exercício 09 – Verificador Matemático -->
+
+<?php
+
+analisarNumero($numero) {
+    $inteiro === is_int($numero);
+    if ($numero === $inteiro) {
+        return "O número é inteiro.";
+    } elseif ($numero/2 === 0) {
+        return "O número é par.";
+    } elseif ($numero/2 !== 0) {
+        return "O número é ímpar.";
+    } elseif ()
+}
+
+echo analisarNumero(5);
